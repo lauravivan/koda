21c993df6863213d1d1e55d34b793ad311100d81
lauravivan <lauvivang@gmail.com>
2026-08-10 23:01:19 -0300

    style(Aside.vue): add rounded border only on right side

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -16,8 +16,8 @@ const props = defineProps<{
 
 <style lang="css">
 .c-aside {
-  border-top-left-radius: 10px;
   border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
   background-color: var(--code-bg);
   padding: 20px;
   height: 80%;
